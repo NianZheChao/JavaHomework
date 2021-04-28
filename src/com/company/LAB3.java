26c2f97ce0471e40ad21437f4829e815baa1c2fc
Nian <nianzhechao@2017.cqut.edu.cn>
2021-04-28 16:13:16 +0800

    init LAB4

--- src/com/company/LAB3.java
+++ src/com/company/LAB3.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import jdk.swing.interop.SwingInterOpUtils;
-
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
---
+++ src/com/company/LAB4.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class LAB4 {
+    class LAB4_1{
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
